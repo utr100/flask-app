bf43c152128321837b327c16b1f40a974dad1747
Ranjan <utkarsh.b.ranjan@accenture.com>
2021-01-15 09:25:16 +0530

    added missing library to requirements.txt

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from flask.ext.heroku import Heroku
+from flask_heroku import Heroku
 
 app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db' # relative path, use //// for abs path
